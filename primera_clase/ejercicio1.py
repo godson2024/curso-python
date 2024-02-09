d56cc9cd537e2eafbce4689ce194c314fd531624
godson2024 <guisrka03@gmail.com>
2024-02-09 11:14:29 -0400

    añadiendo tilde

--- primera_clase/ejercicio1.py
+++ primera_clase/ejercicio1.py
@@ -1,5 +1,5 @@
 #CALCULOS DE SUMATORIAS
 a = int(input("Ingresar un numero: "))
-b = int(input("Ingresar otro numero: "))
+b = int(input("Ingresar otro nùmero: "))
 suma = a+b
 print("El resultado de ",a,"+",b,"es: ",suma)
